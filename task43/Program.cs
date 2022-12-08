1d1ae9f7a71a38b92902e237bc66b371f37f9f0e
Yaroslav <Valso96@mail.ru>
2022-12-08 15:53:24 +0600

    Точка пересечения двух прямых

---
+++ task43/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите значение b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y1 = k1 * x + b1;
+double y2 = k2 * x + b2;
+
+Console.Clear();
+Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2}.");
+Console.WriteLine($"Точка пересечения двух прямых: {y1}; {y2}.");
