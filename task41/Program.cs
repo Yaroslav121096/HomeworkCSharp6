e18310fe49708b5c4ea47e24000427defbdcb548
Yaroslav <Valso96@mail.ru>
2022-12-08 15:52:29 +0600

    Количество чисел больше нуля

---
+++ task41/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Какое количество чисел Вы введёте? ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+int[] numberStorage = new int[numbers];
+for (int i = 0; i < numberStorage.Length; i++)
+{
+    Console.WriteLine($"Введите {i + 1} число: ");
+    numberStorage[i] = Convert.ToInt32(Console.ReadLine());
+}
+int count = 0;
+for (int i = 0; i < numberStorage.Length; i++)
+{
+    if (numberStorage[i] > 0)
+        count++;
+}
+Console.Clear();
+Console.WriteLine($"Заданные числа: {string.Join(", ", numberStorage)}");
+Console.WriteLine($"Количество чисел больше нуля, введенных вами: {count}.");
